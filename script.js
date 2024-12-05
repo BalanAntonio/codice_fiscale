ff958ecf805d46613ebdd6777f6616515b43a027
BALAN ANTONIO FLORIN <12999441@itisrossi.edu.it>
2024-12-05 10:24:29 +0100

    cognome + conta consonanti

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ function calcola(){
     let nome = document.getElementById("nome").value;
     let luogo = document.getElementById("luogo").value;
     let provincia = document.getElementById("provincia").value;
-    let giorno = document.getElementById("giorno").value;
-    let mese = document.getElementById("mese").value;
-    let anno = document.getElementById("anno").value;
+    let data = document.getElementById("data").value;
     
-    console.log(consonante(4,"qeswr"));
+    console.log(conta_consonanti("aeikksak"));
 }
 
 function consonante(ind,str){
@@ -20,20 +18,53 @@ function consonante(ind,str){
         i++;
         if(quant==0) { return str[i-1]; }
     }
-    /*for(i; i <= str.length && quant != ind; i++){
+    return -1;
+}
+
+function conta_consonanti(str){
+    let n = 0;
+    for(let i = 0; i<str.length; i++){
         if(str[i]!="a" && str[i]!="e" && str[i]!="i" && str[i]!="o" && str[i]!="u"){
-            quant += 1
+            n++;
         }
     }
-        
-    
-    while(quant>0){
-        if(str[i-1]!="a" && str[i-1]!="e" && str[i-1]!="i" && str[i-1]!="o" && str[i-1]!="u" && i<str.length){
-            quant -= 1;
-            i+=1;
-        }else{
-            break;
-        }
-    */
-    //return str[i];
+    return n;
 }
+
+function vocale(ind,str){
+    let quant = ind;
+    let i = 0
+    while(i<str.length){
+        if(str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u"){
+            quant-=1;
+        }
+        i++;
+        if(quant==0) { return str[i-1]; }
+    }
+    return -1;
+}
+
+function c_cognome(cognome){
+    let c1 = consonante(1,cognome);
+    let c2 = consonante(2,cognome);
+    let c3 = consonante(3,cognome);
+    let r_cognome = "";
+
+    r_cognome+=c1;
+    if(c2!=-1){
+        r_cognome+=c2;
+        if(c3!=-1){
+            r_cognome+=c3
+        }else{
+            if(vocale(3,cognome)!=-1){
+                r_cognome += vocale(3,cognome);
+            }else{
+                r_cognome += "X"
+            }
+        }
+    }else{
+        r_cognome += vocale(2,cognome);
+    }
+    return r_cognome;
+}
+
